dab86b63cb584f3b9236818a721edabb2ad2ec40
yanan xiao <997079772@qq.com>
2017-03-07 00:54:20 +0800

    first commit

---
+++ js/nowplaying.js
@@ -0,0 +1,14 @@
+// 11.创建 正在热映的控制器的模块 并且创建一个正在热映控制器
+angular.module('movieApp.nowplayingCtrl', [])
+    .controller('nowplayingCtrl', ['$scope', '$http', function($scope, $http) {
+        //1. 实现电影列表的展示功能
+        // 1.创建一个电影列表的属性
+        $scope.movie = {};
+        // 2.通过ajax请求data.json文件 获取数据赋值给movie属性中
+        //$http就是帮我们实现ajax请求的使用方式类似jquery的$.ajax
+        //使用$http.jsonp的方式访问豆瓣的API 访问不了 原因是豆瓣API不支持angular的回调函数的形式
+        // angular的jsonp跨域解决不了豆瓣API的跨域问题
+        $http.jsonp('https://api.douban.com/v2/movie/in_theaters').success(function(data) {
+            $scope.movie = data;
+        });
+    }]);
